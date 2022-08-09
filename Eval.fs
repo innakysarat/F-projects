74391b17af84abada83c33c6100f7445390b1f3e
Inna Tarasyuk <82310490+innakysarat@users.noreply.github.com>
2022-08-09 12:38:25 +0300

    Add files via upload

---
+++ Eval.fs
@@ -0,0 +1,4 @@
+module Assignment6.Eval
+
+open StateMonad
+let hello = [('h', 4); ('e', 1); ('l', 1); ('l', 1); ('O', 1)]
--- Program.fs
+++ Program.fs
@@ -1,68 +1,46 @@
-        // 3 -> sum 5 2 (3 + 2)
-        // 2 -> sum 7 1 (5 + 2)
-        // 1 -> sum 8 0 (7 + 1)
-        // 0 -> 8
-        
-        // m=2 n=3
-        // 2 + 3 + 4 + 5
-        
-        // 5 + 4 + 3 + 2
-        // m = 2; n = 3
-        // m=2+3; n=4; i=2
-        // m=(2+3)+4; n=5; i=1
-        // m=(2+3+4)+5; n=6; i=0
-  
-  
-  // m=0; n=10
-  // 0 + 1 + 2 + 3 + 4 + 5 + ... + 10 = 55
-  
-  // m=2; n=4 -> 2+3+4+5+6
-  // m=3; acc=2; i=3
-  // m=4; acc=5; i=2
-  // acc=9;m=3;i=1
-  
-let rec sum m n =
-    let rec sum_built_in acc m i =
-        match i with
-        | i when i < 0 -> acc
-        | i -> sum_built_in (m+acc) (m+1) (i-1)  
-    sum_built_in 0 m n;;
-  
-let rec length lst =
-    let rec len acc lst =
-        match lst with
-        | [] -> acc
-        | head::tail -> len (acc + 1) tail 
-    len 0 lst;;
 
-let foldBack f lst acc =
-    let rec foldBackAcc lst cont = 
-       match lst with
-            | []      -> cont acc
-            | x :: xs -> foldBackAcc xs (fun r -> cont (f x r))
-    foldBackAcc lst (fun x -> x);;
-    
-//let rec foldBack1 f lst c =
-//    let rec foldBackAcc lst c = 
-//       match lst with
-//            | []      -> c
-//            | x :: xs -> f x (foldBack1 f xs c)
-//    foldBackAcc lst c;;
-    
-//let factA x =
-//    let rec aux x acc =
-//        match x with
-//        | 0 -> acc
-//        | x -> aux (x - 1) (x * acc)
-//    aux x 1;;
-    
-// c is the continuation(function) into which we put any additional calculations
-let factC n =
-    let rec fact n cont =
-        if n = 0
-            then cont 1
-        else
-            fact (n-1) (fun x -> cont(x * n))
-    fact n (fun x -> x);;
+type aExp =
+    | N of int (* Integer literal *)
+    | V of string (* Variable reference *)
 
-#time;
+    | WL (* Word length *)
+    | PV of aExp (* Point value lookup at word index *)
+
+    | Add of aExp * aExp (* Addition *)
+    | Sub of aExp * aExp (* Subtraction *)
+    | Mul of aExp * aExp (* Multiplication *)
+    | Div of aExp * aExp (* NEW: Division *)
+    | Mod of aExp * aExp (* NEW: Modulo *)
+    | CharToInt of cExp (* Cast to integer *)
+
+and cExp =
+    | C of char (* Character literal *)
+    | CV of aExp (* Character lookup at word index *)
+
+    | ToUpper of cExp (* Convert character to upper case *)
+    | ToLower of cExp (* Convert character to lower case *)
+
+    | IntToChar of aExp (* Cast to character *)
+
+
+type bExp =
+    | TT (* True *)
+    | FF (* False *)
+
+    | AEq of aExp * aExp (* Numeric equality *)
+    | ALt of aExp * aExp (* Numeric less than *)
+
+    | Not of bExp (* Boolean not *)
+    | Conj of bExp * bExp (* Boolean conjunction *)
+
+    | IsLetter of cExp (* Check for letter *)
+    | IsDigit of cExp (* Check for digit *)
+
+type stm =
+    | Declare of string (* NEW: Variable declaration *)
+    | Ass of string * aExp (* variable assignment *)
+    | Skip (* Nop *)
+    | Seq of stm * stm (* Sequential composition *)
+    | ITE of bExp * stm * stm (* If-Then-Else statement *)
+    | While of bExp * stm (* While statement *)
+
