52e4a12b26ee8c26d22b1af74bda3fd971e500ab
Inna Tarasyuk <82310490+innakysarat@users.noreply.github.com>
2022-08-09 12:37:22 +0300

    Add files via upload

--- Program.fs
+++ Program.fs
@@ -1,65 +1,68 @@
-//open System
-let rec downto1 x =
-   if x <= 0 then []
-   else x::downto1 (x-1);;
-    
-let rec downto2 =
-   function
-   | x when x <= 0 -> []
-   | x -> x::downto2(x-1);;
-let rec removeOddIdx =
-   function
-      | [] -> []
-      | [x] -> [x]
-      | x::y::z -> x::removeOddIdx z;;
-
-let rec combinePair =
-   function
-      | [] -> []
-      | [x] -> []
-      | x::y::z -> (x, y)::combinePair z;;
-
-type complex = float * float;;
-let mkComplex x y:complex = (x, y);;
-//let mkComplex x y = (x, y):complex;;
-let complexToPair (a,b)= (a,b):complex;;
-let (|+|) ((a,b):complex) ((c,d):complex) = (a+c, b+d);;
-
-let (|*|) ((a,b):complex) ((c,d):complex) = (a*c - b*d, b*c + a*d);;
-
-let (|-|) ((a,b):complex) ((c,d):complex) = (a,b) |+| (-c,-d);;
-
-(*let (|/|) ((a,b):complex) ((c,d):complex) =
-   if a <> 0 || b <> 0 then (a,b) |*| (c/(c*c + d*d),-d/(c*c + d*d))
-   else raise (InvalidOperationException("undefined"));;*)
-let (|/|) ((a,b):complex) ((c,d):complex) =
-   (a,b) |*| (c/(c*c + d*d),-d/(c*c + d*d));;   
-     
-let explode1(s:string) =
-       s.ToCharArray() |> Array.toList;;
-   
-let rec explode2 =
-       function
-          | "" -> [] 
-          | s -> s.Chars(0)::explode2 (s.Remove(0, 1));;
+        // 3 -> sum 5 2 (3 + 2)
+        // 2 -> sum 7 1 (5 + 2)
+        // 1 -> sum 8 0 (7 + 1)
+        // 0 -> 8
+        
+        // m=2 n=3
+        // 2 + 3 + 4 + 5
+        
+        // 5 + 4 + 3 + 2
+        // m = 2; n = 3
+        // m=2+3; n=4; i=2
+        // m=(2+3)+4; n=5; i=1
+        // m=(2+3+4)+5; n=6; i=0
   
-let (.+) (x1:char) (x2:string) = System.Char.ToString(x1) + x2;;
-       
-let implode(lst:char list) =
-   List.foldBack (fun x xs -> string x + xs) lst "";;
+  
+  // m=0; n=10
+  // 0 + 1 + 2 + 3 + 4 + 5 + ... + 10 = 55
+  
+  // m=2; n=4 -> 2+3+4+5+6
+  // m=3; acc=2; i=3
+  // m=4; acc=5; i=2
+  // acc=9;m=3;i=1
+  
+let rec sum m n =
+    let rec sum_built_in acc m i =
+        match i with
+        | i when i < 0 -> acc
+        | i -> sum_built_in (m+acc) (m+1) (i-1)  
+    sum_built_in 0 m n;;
+  
+let rec length lst =
+    let rec len acc lst =
+        match lst with
+        | [] -> acc
+        | head::tail -> len (acc + 1) tail 
+    len 0 lst;;
 
-let implodeRev(lst:char list) =
-   List.fold (fun x xs -> string xs + x) "" lst;;
-   
-let toUpper (s:string) = s.ToCharArray() |> Array.toList |> List.map System.Char.ToUpper |> implode;;
+let foldBack f lst acc =
+    let rec foldBackAcc lst cont = 
+       match lst with
+            | []      -> cont acc
+            | x :: xs -> foldBackAcc xs (fun r -> cont (f x r))
+    foldBackAcc lst (fun x -> x);;
+    
+//let rec foldBack1 f lst c =
+//    let rec foldBackAcc lst c = 
+//       match lst with
+//            | []      -> c
+//            | x :: xs -> f x (foldBack1 f xs c)
+//    foldBackAcc lst c;;
+    
+//let factA x =
+//    let rec aux x acc =
+//        match x with
+//        | 0 -> acc
+//        | x -> aux (x - 1) (x * acc)
+//    aux x 1;;
+    
+// c is the continuation(function) into which we put any additional calculations
+let factC n =
+    let rec fact n cont =
+        if n = 0
+            then cont 1
+        else
+            fact (n-1) (fun x -> cont(x * n))
+    fact n (fun x -> x);;
 
-let rec ack (m, n) =
-   if m = 0 then
-      n + 1
-   elif m > 0 && n = 0 then
-      ack(m-1,1)
-   elif m > 0 && n > 0 then
-      ack(m-1, ack(m, n-1))
-   else
-      0;;
-     
+#time;
